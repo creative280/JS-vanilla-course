2147e98925b2ae1f9a2443436a7d14b4df224686
creative280 <alejandro.taborda280@gmail.com>
2024-09-26 21:38:26 -0500

    Clase fechas - Completada 100%

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,14 @@
+const diaHoy = new Date();
+
+let valor;
+
+valor = diaHoy;
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth();
+valor = diaHoy.getMinutes();
+valor = diaHoy.getHours();
+
+
+
+console.log(valor)
+
